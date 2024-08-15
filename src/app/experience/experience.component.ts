4cc85818beb8cd58f2bf4bd1a079f82ce5639e9e
fjzamora93 <fjzamora93@gmail.com>
2024-08-15 14:41:33 -0400

    update

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
+import { MaterialModule } from '../material/material.module';
+import { DataService } from '../posts/data.service';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.css'
 })
 export class ExperienceComponent {
+    @Input() dataFile!: string;
+    dataList: any[] = [];
 
+    constructor(private dataService: DataService) {}
+
+    ngOnInit(): void {
+        if (this.dataFile) {
+            this.dataService.getData(this.dataFile).subscribe(data => {
+                this.dataList = data;
+            });
+        }
+    }
 }
