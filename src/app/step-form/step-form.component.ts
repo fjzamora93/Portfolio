1a47144647e33ccda3abf6c3e05853aa25b7ec2b
fjzamora93 <fjzamora93@gmail.com>
2024-08-16 14:34:43 -0400

    Update Form

--- src/app/step-form/step-form.component.ts
+++ src/app/step-form/step-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -15,13 +15,29 @@ import {MatStepperModule} from '@angular/material/stepper';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    
   ],
   templateUrl: './step-form.component.html',
   styleUrl: './step-form.component.css'
 })
 export class StepFormComponent {
-    private _formBuilder = inject(FormBuilder);
 
-    firstFormGroup: FormGroup = this._formBuilder.group({firstCtrl: ['']});
-    secondFormGroup: FormGroup = this._formBuilder.group({secondCtrl: ['']});
+
+        // En tu componente
+    firstFormGroup: FormGroup;
+    secondFormGroup: FormGroup;
+    thirdFormGroup: FormGroup;
+
+    constructor(private _formBuilder: FormBuilder) {
+    this.firstFormGroup = this._formBuilder.group({
+        firstCtrl: ['', Validators.required],
+    });
+    this.secondFormGroup = this._formBuilder.group({
+        secondCtrl: ['', [Validators.required, Validators.email]], // Añadido Validators.email
+    });
+    this.thirdFormGroup = this._formBuilder.group({
+        thirdCtrl: ['', Validators.required],
+    });
+}
+
 }
