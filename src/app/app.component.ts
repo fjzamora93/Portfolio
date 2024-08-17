00c7d01c0139c6b929ddc4a79b63c2c35fbec585
fjzamora93 <fjzamora93@gmail.com>
2024-08-17 18:18:38 -0400

    Update Design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import { InvestmentResultComponent } from './investment-result/investment-result.component';
-import { UserInputComponent } from './user-input/user-input.component';
 
-import { InvestmentService } from './investment-result/investment.service';
+
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { GalleryComponent } from "./gallery/gallery.component";
@@ -20,19 +18,12 @@ import { FooterComponent } from './footer/footer.component';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, InvestmentResultComponent,
-    UserInputComponent, PostCreateComponent, PostListComponent, TabsContentComponent,
+  imports: [HeaderComponent, 
+   PostCreateComponent, PostListComponent, TabsContentComponent,
     GalleryComponent, GraphicsComponent, NavBarComponent, NavBarComponent, TabsContentComponent, TableListComponent,
     StepFormComponent, ExperienceComponent, FooterComponent]
 })
 
 export class AppComponent {
-    investment?: InvestmentService;
-
-    onCalculateInvestment(investment: InvestmentService){
-        this.investment = investment;
-        this.investment.calculateInvestmentResults();
-        console.log(`Recibiendo Inversión en el padre: ${JSON.stringify(investment)}`);
-    }
-
+    
 }
--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,17 +1,22 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, Input, inject, input } from '@angular/core';
 import { MaterialModule } from '../material/material.module';
 import { DataService } from '../posts/data.service';
-
+import {MatStepperModule} from '@angular/material/stepper';
+import { FormBuilder } from '@angular/forms';
+import { MatIcon } from '@angular/material/icon';
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [MaterialModule],
+  imports: [MaterialModule, MatStepperModule, MatIcon],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.css'
 })
 export class ExperienceComponent {
     @Input() dataFile!: string;
     dataList: any[] = [];
+    private _formBuilder = inject(FormBuilder);
+
+    
 
     constructor(private dataService: DataService) {}
 
