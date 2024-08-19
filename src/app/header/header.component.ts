679a5bcdc7527c0d538083290b215acd38932a29
fjzamora93 <fjzamora93@gmail.com>
2024-08-19 14:59:55 -0400

    Typing effect

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MaterialModule } from '../material/material.module';
 
 @Component({
@@ -8,6 +8,47 @@ import { MaterialModule } from '../material/material.module';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+    texts: string[] = [
+        'soy desarrollador de software 💻', 
+        'diseño aplicaciones ✏️', 
+        'me encanta el buen café 😁☕'
+    ];
+    currentText: string = '';
+    currentIndex: number = 0;
+    isDeleting: boolean = false;
+    typingSpeed: number = 100;
+    deletingSpeed: number = 50;
+    delayBetweenTexts: number = 2000;
 
+    ngOnInit() {
+        this.typingEffect();
+      }
+    
+      typingEffect() {
+        const fullText = this.texts[this.currentIndex];
+        if (this.isDeleting) {
+          this.currentText = fullText.substring(0, this.currentText.length - 1);
+        } else {
+          this.currentText = fullText.substring(0, this.currentText.length + 1);
+        }
+    
+        let speed = this.typingSpeed;
+    
+        if (this.isDeleting) {
+          speed = this.deletingSpeed;
+        }
+    
+        if (!this.isDeleting && this.currentText === fullText) {
+          speed = this.delayBetweenTexts;
+          this.isDeleting = true;
+        } else if (this.isDeleting && this.currentText === '') {
+          this.isDeleting = false;
+          this.currentIndex = (this.currentIndex + 1) % this.texts.length;
+          speed = this.typingSpeed;
+        }
+    
+        setTimeout(() => this.typingEffect(), speed);
+      }
+    
 }
